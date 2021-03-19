3f8a24f61589ec26223c8c2abcf0e85bccdafbee
BaoTruong1311 <18130262@st.hcmuaf.edu.vn>
2021-03-19 09:59:39 +0700

    java 2

---
+++ Ex-Slide/src/week123/ComputeAverage.java
@@ -0,0 +1,17 @@
+package week123;
+
+import java.util.Scanner;
+
+public class ComputeAverage {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter three number: ");
+		double number1= input.nextDouble();
+		double number2 = input.nextDouble();
+		double number3 = input.nextDouble();
+		
+		double Average = ( number1 + number2 + number3) /3;
+		System.out.println("The average of " + number1 + " " + number2 + " " + number3 + " is "+ Average);
+	}
+
+}
